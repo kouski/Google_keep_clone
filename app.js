97ac97ac62127d760f4c2c311dab10ab7b83f9da
kouski <72550619+kouski@users.noreply.github.com>
2023-11-11 09:28:18 +0100

    Toogle button en js

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 class App{
     constructor(){
         console.log('App works');
+        this.$form = document.querySelector('#form');
+        this.$noteTitle = document.querySelector('#note-title');
+        this.$formButtons = document.querySelector('#form-buttons');
+        
+        
+        this.addEventListeners();
     }
+
+    addEventListeners(){
+        document.addEventListener('click', event=>{
+            this.handleFormClick(event)
+        })
+    }
+    handleFormClick(event){
+        const isFormClicked = this.$form.contains(event.target); 
+        if(isFormClicked){
+            this.openForm()
+        }else{
+            this.closeForm()
+        }
+    }
+
+    openForm(){
+        this.$form.classList.add('form-open');
+        this.$formButtons.style.display='block';
+        this.$noteTitle.style.display= 'block';
+    }
+
+    closeForm(){
+        this.$form.classList.remove('form-open');
+        this.$formButtons.style.display='none';
+        this.$noteTitle.style.display= 'none';
+    }
+    
+    
 }
+        
 
 new App()
