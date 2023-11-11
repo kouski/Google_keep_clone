e11d4b2e66049d89836e293bf22968083d789abb
kouski <72550619+kouski@users.noreply.github.com>
2023-11-11 19:16:29 +0100

    Añadir logica del modal

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
-class App{
-    constructor(){
-        console.log('App works');
-        this.$form = document.querySelector('#form');
-        this.$noteTitle = document.querySelector('#note-title');
-        this.$formButtons = document.querySelector('#form-buttons');
-        
-        
-        this.addEventListeners();
-    }
+class App {
+  constructor() {
+    this.$form = document.querySelector("#form");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$noteText = document.querySelector("#note-text");
+    this.$formButtons = document.querySelector("#form-buttons");
 
-    addEventListeners(){
-        document.addEventListener('click', event=>{
-            this.handleFormClick(event)
-        })
-    }
-    handleFormClick(event){
-        const isFormClicked = this.$form.contains(event.target); 
-        if(isFormClicked){
-            this.openForm()
-        }else{
-            this.closeForm()
-        }
-    }
+    this.addEventListeners();
+  }
 
-    openForm(){
-        this.$form.classList.add('form-open');
-        this.$formButtons.style.display='block';
-        this.$noteTitle.style.display= 'block';
-    }
-
-    closeForm(){
-        this.$form.classList.remove('form-open');
-        this.$formButtons.style.display='none';
-        this.$noteTitle.style.display= 'none';
-    }
+  addEventListeners() {
+    document.body.addEventListener("click", event => {
+      this.handleFormClick(event);
+    });
     
-    
+    this.$form.addEventListener('submit', event => {
+      event.preventDefault(); 
+      const title = this.$noteTitle.value;   
+      const text = this.$noteText.value;   
+      const hasNote = title || text;
+      if (hasNote) {
+        // add note  
+        this.addNote({ title, text });
+      }
+    });
+  }
+
+  handleFormClick(event) {
+    const isFormClicked = this.$form.contains(event.target);
+
+    if (isFormClicked) {
+      this.openForm();
+    } else {
+      this.closeForm();
+    }
+  }
+
+  openForm() {
+    this.$form.classList.add("form-open");
+    this.$noteTitle.style.display = "block";
+    this.$formButtons.style.display = "block";
+  }
+
+  closeForm() {
+    this.$form.classList.remove("form-open");
+    this.$noteTitle.style.display = "none";
+    this.$formButtons.style.display = "none";
+  }
+  
+  addNote()
 }
-        
 
-new App()
+new App();
+        
